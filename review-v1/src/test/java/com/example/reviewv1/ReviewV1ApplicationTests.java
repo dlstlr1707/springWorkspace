e5d208315f69ded17a8ad0176d370d0afa644638
dlstlr1707 <dlstlr1707@naver.com>
2025-02-25 13:59:06 +0900

    Review Git

---
+++ review-v1/src/test/java/com/example/reviewv1/ReviewV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reviewv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
