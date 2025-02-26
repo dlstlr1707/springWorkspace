29adf9839217c312289b9bf4e337ecc4e1b8b8bc
dlstlr1707 <dlstlr1707@naver.com>
2025-02-26 09:58:01 +0900

    2/26 1st class code

---
+++ basic-board-v2/src/main/java/com/example/basicboardv2/controller/DataController.java
@@ -0,0 +1,31 @@
+package com.example.basicboardv2.controller;
+
+import com.example.basicboardv2.dto.DataResponseDTO;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/data")
+public class DataController {
+    private Map<Long, DataResponseDTO> dataStore = new HashMap<>();
+    private Long idCnt = 1L;
+    @PostConstruct
+    public void initDataSource(){
+        dataStore.put(idCnt++, new DataResponseDTO(1L,"Item 1",100));
+        dataStore.put(idCnt++, new DataResponseDTO(1L,"Item 1",100));
+        dataStore.put(idCnt++, new DataResponseDTO(1L,"Item 1",100));
+        dataStore.put(idCnt++, new DataResponseDTO(1L,"Item 1",100));
+        dataStore.put(idCnt++, new DataResponseDTO(1L,"Item 1",100));
+    }
+
+    @GetMapping("/{id}")
+    public DataResponseDTO getData(@PathVariable Long id){
+        return dataStore.get(id);
+    }
+}
---
+++ basic-board-v2/src/main/java/com/example/basicboardv2/dto/DataRequestDTO.java
@@ -0,0 +1,9 @@
+package com.example.basicboardv2.dto;
+
+import lombok.Getter;
+
+@Getter
+public class DataRequestDTO {
+    private String name;
+    private int value;
+}
