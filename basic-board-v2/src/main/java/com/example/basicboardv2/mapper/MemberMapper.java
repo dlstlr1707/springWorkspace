cc3a3f4eae68f67de472da02db0ebad7d86a6072
dlstlr1707 <dlstlr1707@naver.com>
2025-03-06 09:45:21 +0900

    25/03/06 1st class code

---
+++ basic-board-v2/src/main/java/com/example/basicboardv2/mapper/MemberMapper.java
@@ -0,0 +1,9 @@
+package com.example.basicboardv2.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MemberMapper {
+
+
+}
---
+++ basic-board-v2/src/main/java/com/example/basicboardv2/service/MemberService.java
@@ -0,0 +1,9 @@
+package com.example.basicboardv2.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+
+
+}
