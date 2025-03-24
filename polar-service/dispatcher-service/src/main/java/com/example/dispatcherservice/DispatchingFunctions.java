2e48eef8d9bd730c3b171c6f8131caa6e3406187
dlstlr1707 <dlstlr1707@naver.com>
2025-03-24 11:42:27 +0900

    25/03/24 3st class code

--- polar-service/dispatcher-service/src/main/java/com/example/dispatcherservice/DispatchingFunctions.java
+++ polar-service/dispatcher-service/src/main/java/com/example/dispatcherservice/DispatchingFunctions.java
@@ -1,39 +1,40 @@
 package com.example.dispatcherservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Flux;
 
 import java.util.function.Function;
-import java.util.logging.Logger;
+
 
 @Configuration
 public class DispatchingFunctions {
 
-    private static final Logger log = Logger.getLogger(DispatchingFunctions.class.getName());
+    private static final Logger log = LoggerFactory.getLogger(DispatchingFunctions.class);
 
     @Bean
-    public Function<OrderAcceptedMessage, Long> pack(){
+    public Function<OrderAcceptedMessage, Long> pack() {
         return orderAcceptedMessage -> {
-            log.info("The order with id {} is packed. " + orderAcceptedMessage.orderId());
+            log.info("The order with id {} is packed.", orderAcceptedMessage.orderId());
             return orderAcceptedMessage.orderId();
         };
-//        아래 코드는 위의 코드와 같은 내용임
 //        return new Function<OrderAcceptedMessage, Long>() {
-//
 //            @Override
 //            public Long apply(OrderAcceptedMessage orderAcceptedMessage) {
-//                log.info("The order with id {} is packed. " + orderAcceptedMessage.orderId());
+//                log.info("The order with id {} is packed." + orderAcceptedMessage.orderId());
 //                return orderAcceptedMessage.orderId();
 //            }
 //        };
     }
 
     @Bean
-    public Function<Flux<Long>, Flux<OrderAcceptedMessage>> label(){
+    public Function<Flux<Long>, Flux<OrderAcceptedMessage>> label() {
         return orderFlux -> orderFlux.map( orderId -> {
-            log.info("The order with id {} is labeled. " + orderId);
+            log.info("The order with id {} is labeled.", orderId);
             return new OrderAcceptedMessage(orderId);
         });
     }
+
 }
--- polar-service/order-service/src/main/java/com/example/orderservice/order/domain/OrderService.java
+++ polar-service/order-service/src/main/java/com/example/orderservice/order/domain/OrderService.java
@@ -2,20 +2,25 @@ package com.example.orderservice.order.domain;
 
 import com.example.orderservice.book.Book;
 import com.example.orderservice.book.BookClient;
+import com.example.orderservice.event.OrderAcceptedMessage;
+import com.example.orderservice.event.OrderDispatchedMessage;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import static com.example.orderservice.order.domain.OrderStatus.ACCEPTED;
-import static com.example.orderservice.order.domain.OrderStatus.REJECTED;
+import static com.example.orderservice.order.domain.OrderStatus.*;
 
 @Service
 @RequiredArgsConstructor
 public class OrderService {
-
+    private final static Logger log = LoggerFactory.getLogger(OrderService.class);
     private final OrderRepository orderRepository;
     private final BookClient bookClient;
+    private final StreamBridge streamBridge;
 
     public Flux<Order> findAll() {
         return orderRepository.findAll();
@@ -28,9 +33,41 @@ public class OrderService {
                 .defaultIfEmpty(buildRejectedOrder(isbn,quantity))
                 // Mono<Mono<Order>>를 Mono<Order>로 여러개의 Mono를 합쳐 준다.
                 .flatMap(orderRepository::save)
-                ;
+                .doOnNext(this::publishOrderAcceptedEvent);
     }
-    
+
+    public Flux<Order> consumerOrderDispatchedEvent(Flux<OrderDispatchedMessage> orderDispatchedMessageFlux) {
+        return orderDispatchedMessageFlux
+                .flatMap(orderDispatchedMessage -> orderRepository.findById(orderDispatchedMessage.orderId()))
+                .map(this::buildDispatchedOrder);
+    }
+
+    private void publishOrderAcceptedEvent(Order order) {
+        if(!order.status().equals(ACCEPTED)) {
+            return;
+        }
+
+        OrderAcceptedMessage orderAcceptedMessage = new OrderAcceptedMessage(order.id());
+        log.info("Publishing order accepted event id : {}", orderAcceptedMessage.orderId());
+
+        boolean result = streamBridge.send("acceptOrder-out-0", orderAcceptedMessage);
+        log.info("Result of sending order accepted event : {}, id : {}", result, orderAcceptedMessage.orderId());
+    }
+
+    private Order buildDispatchedOrder(Order existingOrder){
+        return Order.builder()
+                .id(existingOrder.id())
+                .bookIsbn(existingOrder.bookIsbn())
+                .bookName(existingOrder.bookName())
+                .quantity(existingOrder.quantity())
+                .bookPrice(existingOrder.bookPrice())
+                .status(DISPATCHED)
+                .createdDate(existingOrder.createdDate())
+                .lastModifiedDate(existingOrder.lastModifiedDate())
+                .version(existingOrder.version())
+                .build();
+    }
+
     private static Order buildAcceptedOrder(Book book, int quantity) {
         return Order.builder()
                 .bookIsbn(book.isbn())
